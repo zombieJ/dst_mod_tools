5e64849f3bcb775b5be6d31de37024c76b5a3795
jiljiang <jiljiang@ebay.com>
2017-06-19 15:59:18 +0800

    add template

---
+++ GoldenSpear/modinfo.lua
@@ -0,0 +1,17 @@
+name = "Golden Spear"
+description = "Spear made of gold, more durable and sharper than the one made with flint."
+author = "[BSy]Jupiter"
+version = "1.0"
+
+forumthread = ""
+
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
+api_version = 6
+
+
+icon_atlas = "goldenspear.xml"
+icon = "goldenspear.tex"
